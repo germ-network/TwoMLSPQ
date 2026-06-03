ffefd439c10f23f3d073735bdda79d77f6a46a30
Mark Xue <mark@germ.network>
2026-06-03 11:11:05 -0700

    start out with the shared protocol

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
 	name: "AbstractTwoMLS",
+	platforms: [.iOS(.v17), .macOS(.v15)],
 	products: [
 		// Products define the executables and libraries a package produces, making them visible to other packages.
 		.library(
@@ -12,11 +13,20 @@ let package = Package(
 			targets: ["AbstractTwoMLS"]
 		)
 	],
+	dependencies: [
+		.package(
+			url: "https://github.com/germ-network/autonomous-comm-protocol.git",
+			from: "1.2.0"
+		)
+	],
 	targets: [
 		// Targets are the basic building blocks of a package, defining a module or a test suite.
 		// Targets can depend on other targets in this package and products from dependencies.
 		.target(
-			name: "AbstractTwoMLS"
+			name: "AbstractTwoMLS",
+			dependencies: [
+				.product(name: "CommProtocol", package: "autonomous-comm-protocol")
+			]
 		),
 		.testTarget(
 			name: "AbstractTwoMLSTests",
--- Sources/AbstractTwoMLS/AbstractTwoMLS.swift
+++ Sources/AbstractTwoMLS/AbstractTwoMLS.swift
@@ -1,2 +1,89 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  AbstractTwoMLS.swift
+//  CoreAppLogic
+//
+//  Created by Mark @ Germ on 6/2/26.
+//
+
+import CommProtocol
+import Foundation
+
+//abstracts the TwoMLS API surface that PersistedTwoMLS depends on,
+//so that we can sub out different implementations (classical to PQ)
+public enum AbstractTwoMLS {
+	public typealias ClientID = Data
+	public typealias GroupID = Data
+	//should be 32 bytes
+	public typealias RendezvousID = Data
+
+	public protocol Session {
+		var proposalContext: TypedDigest? { get }
+		//this is an exported secret with width 32 bytes
+		var sendRendezvous: RendezvousID? { get throws }
+
+		associatedtype Archive: Codable, Sendable
+		init(archive: Archive) throws
+		var archive: Archive { get throws }
+
+		associatedtype PrepareEncryptResult: PrepareEncryptResultProtocol
+		func prepareToEncrypt(
+			proposing: ClientID?
+		) throws -> PrepareEncryptResult?
+		associatedtype EncryptResult: EncryptResultProtocol
+		func encrypt(appMessage: Data) throws -> EncryptResult
+		func processIncoming(ciphertext: Data) throws -> DecryptResult?
+		func queueProposal(digest: TypedDigest) throws
+		func forwarded(headerDecrypted: Data) throws -> MLSSenderMessage?
+		//resolve if this is the receive group or the session id
+		func shouldListenOn() throws -> (GroupID, [UInt64: RendezvousID])
+
+		//the concrete types are defined in the implementations so we avoid
+		//redefining them
+		associatedtype MLSSenderMessage: MLSSenderMessageProtocol
+		associatedtype DecryptResult: DecryptResultProtocol
+		where DecryptResult.SenderMessage == MLSSenderMessage
+
+	}
+
+	public protocol PrepareEncryptResultProtocol {
+		var proposalHash: TypedDigest { get }
+		var commitedRemoteClientId: ClientID? { get }
+		var didCommit: Bool { get }
+	}
+
+	public protocol EncryptResultProtocol {
+		var cipherText: Data { get }
+		var sender: ClientID { get }
+		var recipient: ClientID { get }
+		var epoch: UInt64 { get }
+	}
+
+	//pass the sender client identity along with the appmessage
+	public protocol MLSSenderMessageProtocol: Sendable {
+		var appMessageData: Data { get }
+		var senderClientId: ClientID { get }
+		var epoch: UInt64 { get }
+	}
+
+	public protocol DecryptResultProtocol: Sendable {
+		associatedtype SenderMessage: MLSSenderMessageProtocol
+		associatedtype QueuedRemoteProposal: QueuedRemoteProposalProtocol
+		associatedtype CommitResult: CommitResultProtocol
+
+		var applicationMessage: SenderMessage? { get }
+		var proposal: QueuedRemoteProposal? { get }
+		var remoteCommit: CommitResult? { get }
+	}
+
+	public protocol QueuedRemoteProposalProtocol: Sendable {
+		var digest: TypedDigest { get }
+		var sender: ClientID { get }
+		var proposing: ClientID { get }
+		var context: TypedDigest { get }
+	}
+
+	public protocol CommitResultProtocol: Sendable {
+		var newSender: ClientID? { get }
+		var newRecipient: ClientID { get }
+	}
+}
